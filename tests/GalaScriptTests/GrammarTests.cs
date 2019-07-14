d29535d5fb0fc75aea25e94e0622d34f7ae4bef1
Chino Chang <chino@joymoe.com>
2019-07-14 20:52:08 +0800

    Add test.gs

--- tests/GalaScriptTests/GrammarTests.cs
+++ tests/GalaScriptTests/GrammarTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.IO;
+using System.Linq;
 using GalaScript;
 using GalaScript.Evaluators;
 using GalaScript.Interfaces;
@@ -164,31 +165,7 @@ result = ""hello""
         [Test]
         public void TestScript()
         {
-            var exps = _parser.Prepare(@"
-*label1
-[add 1 1]:$va
-[pop ebx]
-[goto *label3]
-
-* label2
-[add ret 2]
-[goto *end]
-
-*start # start is a special label
-# start here
-- Hello World
-- GalaScript
-+ Test
-[add 2 2]
-[push ebx]
-[goto *label1]
-
-* label3
-[add $va 2]
-[goif *label2]
-
-*end
-");
+            var exps = _parser.Prepare(File.ReadAllText("test.gs"));
 
             Assert.AreEqual(19, exps.Count());
         }
