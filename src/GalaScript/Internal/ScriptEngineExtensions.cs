8fb401fa3aee29a1f91f0e6a7f034872fe9a10ad
Chino Chang <chino@joymoe.com>
2019-07-15 20:34:37 +0800

    cleanup

--- src/GalaScript/Internal/ScriptEngineExtensions.cs
+++ src/GalaScript/Internal/ScriptEngineExtensions.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Reflection;
-using System.Linq;
+﻿using System.Linq;
 using System.Linq.Expressions;
+using System.Reflection;
 using GalaScript.Interfaces;
 
-namespace GalaScript
+namespace GalaScript.Internal
 {
     public static class ScriptEngineExtensions
     {
--- src/GalaScript/ScriptEngine.cs
+++ src/GalaScript/ScriptEngine.cs
@@ -21,7 +21,7 @@ namespace GalaScript
         private IDropOutStack<object> _ebx = new DropOutStack<object>(10);
         private Dictionary<string, object> _aliases = new Dictionary<string, object>();
 
-        private static readonly object _void = new object();
+        private static readonly object Void = new object();
 
         private void PrepareOperations()
         {
@@ -48,8 +48,7 @@ namespace GalaScript
 
             var callerExpr = Expression.Parameter(typeof(IScriptEvaluator), "caller");
             var paraExpr = Expression.Parameter(typeof(object[]), "obj");
-            var callExprs = new List<Expression>(funcParameters.Length);
-
+            var callExpr = new List<Expression>(funcParameters.Length);
 
             var converter = typeof(Convert).GetMethod("ChangeType", new[] { typeof(object), typeof(Type) });
 
@@ -59,25 +58,27 @@ namespace GalaScript
                 var pType = info.ParameterType;
                 if (pType.IsValueType || pType == typeof(string))
                 {
+                    // ReSharper disable AssignNullToNotNullAttribute
                     var convert = Expression.Call(converter, Expression.ArrayIndex(paraExpr, Expression.Constant(objIndex++)), Expression.Constant(pType));
-                    callExprs.Add(Expression.Convert(convert, pType));
+                    // ReSharper restore AssignNullToNotNullAttribute
+                    callExpr.Add(Expression.Convert(convert, pType));
                 }
                 else if (pType == typeof(IScriptEvaluator))
                 {
-                    callExprs.Add(callerExpr);
+                    callExpr.Add(callerExpr);
                 }
                 else if (Array.IndexOf(funcParameters, info) == funcParameters.Length - 1 && pType.BaseType == typeof(Array) && pType.GetElementType() is Type elementType)
                 {
-
+                    // ReSharper disable PossibleNullReferenceException
                     var ofTypeMethod = typeof(Enumerable).GetMethod("OfType").MakeGenericMethod(elementType);
                     var toArrayMethod = typeof(Enumerable).GetMethod("ToArray").MakeGenericMethod(elementType);
                     var skipMethod = typeof(Enumerable).GetMethod("Skip").MakeGenericMethod(elementType);
+                    // ReSharper restore PossibleNullReferenceException
 
                     if (elementType.IsValueType || elementType == typeof(string))
                     {
                         Func<object[], int, Type, IEnumerable<object>> arrayConverter = (obj, start, type) => obj.Skip(start).Select(o => Convert.ChangeType(o, type));
 
-                        // objs.Skip(objIndex).Select(0=>Covert.ChangeType(o, type)).OfType<elementType>().ToArray()
                         var convertCallExpr = Expression.Call(toArrayMethod,
                             Expression.Call(ofTypeMethod,
                                 Expression.Call(Expression.Constant(arrayConverter.Target), arrayConverter.Method,
@@ -85,11 +86,11 @@ namespace GalaScript
                                     Expression.Constant(objIndex),
                                     Expression.Constant(elementType))));
 
-                        callExprs.Add(convertCallExpr);
+                        callExpr.Add(convertCallExpr);
                     }
                     else if(elementType == typeof(object))
                     {
-                        callExprs.Add(Expression.Call(toArrayMethod, Expression.Call(skipMethod, paraExpr, Expression.Constant(objIndex))));
+                        callExpr.Add(Expression.Call(toArrayMethod, Expression.Call(skipMethod, paraExpr, Expression.Constant(objIndex))));
                     }
                 }
                 else
@@ -98,28 +99,25 @@ namespace GalaScript
                 }
             }
 
-            var body = Expression.Call(func.Method.IsStatic ? null : Expression.Constant(func.Target), func.Method, callExprs);
+            var body = Expression.Call(func.Method.IsStatic ? null : Expression.Constant(func.Target), func.Method, callExpr);
 
             var isAction = func.Method.ReturnType == typeof(void);
 
             Func<IScriptEvaluator, object[], object> fun;
             if(isAction)
             {
-                var _actionCaller = Expression.Lambda<Action<IScriptEvaluator, object[]>>(body, callerExpr, paraExpr).Compile();
+                var actionCaller = Expression.Lambda<Action<IScriptEvaluator, object[]>>(body, callerExpr, paraExpr).Compile();
                 fun = (caller, obj) =>
                 {
-                    _actionCaller(caller, obj);
-                    return _void;
+                    actionCaller(caller, obj);
+                    return Void;
                 };
             }
             else
             {
                 var boxed = Expression.Convert(body, typeof(object));
-                var _funcCaller = Expression.Lambda<Func<IScriptEvaluator, object[], object>>(boxed, callerExpr, paraExpr).Compile();
-                fun = (caller, obj) =>
-                {
-                    return _funcCaller(caller, obj);
-                };
+                var funcCaller = Expression.Lambda<Func<IScriptEvaluator, object[], object>>(boxed, callerExpr, paraExpr).Compile();
+                fun = (caller, obj) => funcCaller(caller, obj);
             }
 
             _functions[name] = fun;
@@ -132,16 +130,18 @@ namespace GalaScript
                 caller = _script;
             }
 
-             var result = _functions[name](caller, arguments);
+            var result = _functions[name](caller, arguments);
 
-            if(result != _void)
+            if (result == Void)
             {
-                caller.SetAlias("ret", result);
+                return result;
+            }
 
-                if (name != "push" && name != "peek" && name != "pop" && name != "goto" && name != "goif")
-                {
-                    EngineOperations.Push(caller, "eax");
-                }
+            caller.SetAlias("ret", result);
+
+            if (name != "push" && name != "peek" && name != "pop" && name != "goto" && name != "goif")
+            {
+                EngineOperations.Push(caller, "eax");
             }
 
             return result;
